9f3f96b641a719770cccdfe1c3e2f1aaef024bb4
siddhu2502 <dsiddharth25@gmail.com>
2025-02-15 21:43:10 +0530

    remove unused functions in utils

--- include/Utils.h
+++ include/Utils.h
@@ -6,19 +6,6 @@
 #include <filesystem>
 #include <unistd.h>
 
-
-// Callback function to handle window resizing
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-};
-
-// Process input
-void processInput(GLFWwindow* window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-}
-
 std::string getExecutableDir() {
     char buffer[1024];
     ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
